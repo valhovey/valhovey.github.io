442d097e19dfc0043e2267c630fd83773ea5a5be
valhovey <khoveyc@gmail.com>
2026-01-01 23:09:37 -0700

    Added resolutions for 2026

---
+++ Resolutions/_next/static/EF6J1Ak9SOW-D5ExGTk-P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b24bdecaaffd0e44.css","static/chunks/pages/index-368dd08a650ff37f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
