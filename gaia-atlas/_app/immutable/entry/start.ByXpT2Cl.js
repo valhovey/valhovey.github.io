f412406b1c8bb2fa471286cb93dd5ba6535d5e2b
valhovey <khoveyc@gmail.com>
2026-05-26 22:39:25 -0600

    Fixing gaia atlas deploy

--- gaia-atlas/_app/immutable/entry/start.ByXpT2Cl.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/-RztclvF.js";import{m as t}from"../chunks/BMSz2tRg.js";export{t as load_css,r as start};
--- gaia-atlas/build/_app/immutable/chunks/CgW0znJV.js
+++
@@ -1,1 +0,0 @@
-import{b5 as o,Z as t,b3 as c}from"./lxDbxSYs.js";function f(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function u(e){t===null&&f(),o(()=>{const n=c(e);if(typeof n=="function")return n})}export{u as o};
