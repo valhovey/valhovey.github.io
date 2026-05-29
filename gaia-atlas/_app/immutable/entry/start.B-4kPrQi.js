d69a423d2a780a757e7747b4bdd65d8172fcf4b8
valhovey <khoveyc@gmail.com>
2026-05-28 23:36:08 -0600

    Updating star info

--- gaia-atlas/_app/immutable/entry/start.B-4kPrQi.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/DaOZaExh.js";import{m as t}from"../chunks/i3GHsEpL.js";export{t as load_css,r as start};
---
+++ gaia-atlas/_app/immutable/entry/start.CNoC9LtM.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/BXF3e_-Q.js";import{m as t}from"../chunks/B0o6c2BJ.js";export{t as load_css,r as start};
