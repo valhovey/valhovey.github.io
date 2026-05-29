7121a1571be0fd840861e8d49ed400d9ddd430ec
valhovey <khoveyc@gmail.com>
2026-05-28 18:42:47 -0600

    Fixing zoom bug

---
+++ gaia-atlas/_app/immutable/entry/start.B-4kPrQi.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/DaOZaExh.js";import{m as t}from"../chunks/i3GHsEpL.js";export{t as load_css,r as start};
--- gaia-atlas/_app/immutable/entry/start.CaFGhoCL.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/-HHpoIRQ.js";import{m as t}from"../chunks/DsP7jdVF.js";export{t as load_css,r as start};
