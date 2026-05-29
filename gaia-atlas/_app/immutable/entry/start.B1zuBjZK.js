30f12b4390359c9a1f1ec8d3ef5b960e650c9873
valhovey <khoveyc@gmail.com>
2026-05-29 08:30:36 -0600

    Zoom level and title update

---
+++ gaia-atlas/_app/immutable/entry/start.B1zuBjZK.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/14vK50ri.js";import{m as t}from"../chunks/DscJvNiY.js";export{t as load_css,r as start};
--- gaia-atlas/_app/immutable/entry/start.CgUT_lQR.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/B4WWO7RF.js";import{m as t}from"../chunks/fV_aOMK4.js";export{t as load_css,r as start};
