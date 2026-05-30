71f2dcd60e539668f0ebe3df553d532ad86162fd
valhovey <khoveyc@gmail.com>
2026-05-29 23:20:34 -0600

    Updating VR version again

--- gaia-atlas-vr/_app/immutable/entry/start.D9C_N7S_.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/DUaPp9H2.js";import{m as t}from"../chunks/bwfxaxu7.js";export{t as load_css,r as start};
---
+++ gaia-atlas-vr/_app/immutable/entry/start.tbE8gq5J.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/DZW6aYjQ.js";import{m as t}from"../chunks/DV_b-1wh.js";export{t as load_css,r as start};
