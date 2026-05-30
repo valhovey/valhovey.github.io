1a703d0c1d49fa9ced9d88f6df1d843cb87f059e
valhovey <khoveyc@gmail.com>
2026-05-29 22:03:11 -0600

    Fixing base path

--- gaia-atlas-vr/_app/immutable/entry/start.NLQePIqm.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/C9A6JUoB.js";import{m as t}from"../chunks/Cjfo8lpi.js";export{t as load_css,r as start};
---
+++ gaia-atlas-vr/_app/immutable/entry/start.ZbabkqFr.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/DoT2-Hla.js";import{m as t}from"../chunks/D7b-EFrb.js";export{t as load_css,r as start};
