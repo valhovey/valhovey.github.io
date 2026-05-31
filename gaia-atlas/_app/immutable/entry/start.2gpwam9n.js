bfeabbbff2eab90b595c4c759298d163af17a323
valhovey <khoveyc@gmail.com>
2026-05-30 23:42:33 -0600

    Adding Proxima Centauri info box

---
+++ gaia-atlas/_app/immutable/entry/start.2gpwam9n.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/BGujFdMY.js";import{m as t}from"../chunks/6peDBEzs.js";export{t as load_css,r as start};
--- gaia-atlas/_app/immutable/entry/start.CRJhnytQ.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/nPqAxv4k.js";import{m as t}from"../chunks/Dg6RwJdf.js";export{t as load_css,r as start};
