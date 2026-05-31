b424fa58f0a6ad22157a8c80ab575cb5357f05db
valhovey <khoveyc@gmail.com>
2026-05-31 09:48:41 -0600

    Fixing zoom scale

--- gaia-atlas/_app/immutable/entry/start.2gpwam9n.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/BGujFdMY.js";import{m as t}from"../chunks/6peDBEzs.js";export{t as load_css,r as start};
---
+++ gaia-atlas/_app/immutable/entry/start.DFkWh8Ty.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/TCMRYeiz.js";import{m as t}from"../chunks/BFVYwCya.js";export{t as load_css,r as start};
